3e136c5494f90944e60f244d43d797cda7ead776
AQuintario <adrian.quintario@okuant.com>
2020-04-13 13:05:48 +0200

    init.R doing tested, output as a symlink

--- src/init.R
+++ src/init.R
@@ -1,15 +1,52 @@
+# Basics ----------------------------------------------------------------------------------------------------------
+library(okutools)
+
+proj_name <- list.files(".", pattern = "*.Rproj")
+proj_name <- gsub("\\.Rproj", "", proj_name)
+
+data_path <- Sys.getenv("DATA_PATH")
+
 # Populate data/raw -----------------------------------------------------------------------------------------------
-#' Run once to populate your project/data/raw folder with symbolic links to files required
-#' by your project, which physically are en your synchronized OneDrive folder.
-#' Don't forget to change path to this
-#' Uncomment or delete.
-files <- c(
-  "example.csv",
-  "another_example.RDS",
-  exp_inmuebles.RDS
- )
-dest <- "./data/raw"
-target <- "path/to/your/DATA/"
-lapply(files, function(f) R.utils::createLink(link = paste0(dest, f),
-                                              target = paste0(target, f)))
-rm(files, dest, target)
+configs <- purrr::map(
+  list.files(path = "./cfg", ".yml", full.names = T),
+  load_config
+)
+configs <- purrr::map(
+  configs,
+  `$`, get
+)
+
+files <- purrr::invoke_map(configs, "datasets")
+files <- unlist(files)
+files <- unique(files)
+
+dataraw_link_dir <- "./data/raw"
+lapply(files, function(f) R.utils::createLink(link = file.path(dataraw_link_dir, f),
+                                              target = file.path(data_path, f)))
+
+
+# Prepare data/processed ------------------------------------------------------------------------------------------
+proc_data_physdir <- file.path(data_path, proj_name, "processed_data")
+dir.create(proc_data_physdir, recursive = T, showWarnings = FALSE)
+
+procdata_dest <- "./data/processed"
+R.utils::createLink(link = procdata_dest,
+                    target = proc_data_physdir)
+# file.symlink(from = proc_data_physdir,
+#              to = procdata_dest)
+saveRDS(proc_data_physdir, "data/processed/proc_data_physdir.RDS")
+
+
+# Prepare output --------------------------------------------------------------------------------------------------
+output_physdir <- file.path(data_path, proj_name, "output")
+dir.create(output_physdir, showWarnings = FALSE)
+
+output_dest <- "./output"
+R.utils::createLink(link = output_dest,
+                    target = proc_data_physdir)
+
+
+
+rm(configs, files, dataraw_link_dir, proc_data_physdir, procdata_dest)
+
+
