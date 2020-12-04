6d5c91b328682b4b20334fc82890ddecd79def25
AQuintario <adrian.quintario@okuant.com>
2020-12-04 09:01:20 +0100

    Dummy 5

--- main.R
+++ main.R
@@ -3,7 +3,7 @@
 #'   "okuanters/okutools",
 #'   auth_user = rstudioapi::askForPassword("Bitbucket username"),
 #'   password = rstudioapi::askForPassword("Bitbucket password")
-#' )
+#' ) 
 
 #' Usual libraries. Uncomment or delete
 # library(okutools)
