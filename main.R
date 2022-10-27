608bc91f138bced013813bf449c49a3fdbeccf42
AQuintario <adrian.quintario@okuant.com>
2022-10-27 13:57:56 +0200

    Init

--- main.R
+++ main.R
@@ -1,15 +1,1 @@
-#' For template use only. Uncomment or delete.
-#' devtools::install_bitbucket(
-#'   "okuanters/okutools",
-#'   auth_user = rstudioapi::askForPassword("Bitbucket username"),
-#'   password = rstudioapi::askForPassword("Bitbucket password")
-#' ) 
 
-#' Usual libraries. Uncomment or delete
-# library(okutools)
-# library(data.table)
-# library(ggplot2)
-
-# Read and write data like this:
-# inputdata <- readRDS("data/raw/inputdata.RDS")
-# saveRDS(object, "data/processed/object.RDS")
